cbe084e4bca1e0d9d41b681773011eb640d6e8b0
Max <eekumax@gmail.com>
2020-01-26 17:14:42 +0700

    shopping

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-new Vue ({
+new Vue({
   el:"#app",
   data:{
-    image:''
-  },
-  methods:{
-    onFileChange(e){
-      var files =e.target.files || e.dataTransfer.files;
-      if(!files.length)
-        return;
-      this.createImage(files[0]);
-      console.log("เลือกไฟล์แล้วววววว");
-    },
-
-
-    createImage(file){
-        var image=new Image();
-        var reader=new FileReader();
-        var vm=this;
-        reader.onload=(e)=>{
-          vm.image=e.target.result;
-        };
-        reader.readAsDataURL(file);
-    }
-
+    items:[
+      {
+        name:"กาแฟ",
+        price:15,
+        url:"http://placehold.it/400x400",
+        active:false
+      },
+      {
+        name:"เครื่องซักผ้า",
+        price:500,
+        url:"http://placehold.it/400x400",
+        active:false
+      },
+      {
+        name:"หนังสือการ์ตูน",
+        price:250,
+        url:"http://placehold.it/400x400",
+        active:false
+      },
+    ]
   }
 })
